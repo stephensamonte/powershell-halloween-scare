d1c16ecc73d5fabca82c424bafeec48d9667ae2c
Stephen Samonte Tan <30998408+stephensamonte@users.noreply.github.com>
2018-10-31 17:50:51 -0400

    Updated code to play a scary video
    Canges:
    - Added scary video
    - Simplified code to play a video

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # powershell halloween scare
+
+Description:  
+This is a Halloween prank program that launches a scary video after a predetermined time (40 minutes).
+
+Usecase: people are watching a video and BAM they will get scared by the random scary 
+	video that launches after 40 minutes. 
+
--- YoutubeVidOnTimer.ps1
+++ YoutubeVidOnTimer.ps1
@@ -7,21 +7,37 @@
 
 #----------------------------------------------------
 
-#starts the main powershell function in a hidden view
-PowerShell.exe -windowstyle hidden{
-sleep 5; 
- 
-# This starts playing a sound in without showing the new window. 
-# More accurately this starts playing sound in a new window that is hidden from view
-start -WindowStyle hidden ".\Assets\Scary-Beat-(nightmare).mp3"
+### Directions: run this program in Weindows PowerShell ISE so that it 
+### launches the video ontop of what ever you are doing on the computer. 
+
+sleep 5
+
+#Start-Process 'C:\Program Files (x86)\Windows Media Player\wmplayer.exe'
+start ".\Assets\Follow The Red Dot 3 Scare - YouTube.mp4" -WindowStyle Maximized
+
+# ----------------------------------------------------
+
+### Other commands: 
 
 # launches an image from the assets folder 
-start ".\Assets\download.jpg"
+#start ".\Assets\download.jpg"
+# start ".\Assets\download.jpg" -WindowStyle Maximized
+
+# ----------------------------------------------------
+
+#starts the main powershell function in a hidden view
+#PowerShell.exe -windowstyle hidden{
+
+
+# This starts playing a sound in without showing the new window. 
+# More accurately this starts playing sound in a new window that is hidden from view
+# start -WindowStyle hidden ".\Assets\Scary-Beat-(nightmare).mp3"
+
+# launches an image from the assets folder 
+# start ".\Assets\download.jpg"
 
 # launches a video on youtube
-start "https://www.youtube.com/embed/JAhEcrP5f5M?rel=0&amp;autoplay=1;fs=0;autohide=0;hd=0;"
+# start "https://www.youtube.com/embed/JAhEcrP5f5M?rel=0&amp;t=9;autoplay=1;fs=0;autohide=0;hd=0;controls=0;"
 
-start ".\Assets\Scary-Beat-(nightmare).mp3"
-
-
-}
+# start -windowstyle Normal".\Assets\Scary-Beat-(nightmare).mp3"
+#}
